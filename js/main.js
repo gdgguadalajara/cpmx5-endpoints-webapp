658bce012dc12da7eca4c8355cdcf1c17e1c62da
Josue David Hernandez Gutierrez <megadavidtor@gmail.com>
2014-06-25 01:44:17 -0500

    make it more configurable

--- js/main.js
+++ js/main.js
@@ -1,29 +1,27 @@
+var ROOT_PATH = 'https://your_app_id.appspot.com/_ah/api';
+
 app = angular.module('app', []);
-
-
 
 app.controller('AppCtrl', ['$scope', '$window', function($scope, $window){
 	ctx = {
 		user_id: "",
-		google_locations: [
-
-		]
+		google_locations: []
 	};
 
 	$window.init = function() {
-		$scope.$apply($scope.load_guestbook_lib);
+		$scope.$apply(ctx.set_location);
 	};
 
 	ctx.get_data = function(){
 		gapi.client.coordinates.leer_coordenadas({id_usuario: ctx.user_id}).execute(function(resp){
 			console.log(resp);
 		});
-	}
+	};
 	
 	ctx.set_location = function(){
 		gapi.client.load('coordinates', 'v1', function() {
 			ctx.get_data();
-		}, 'https://your_app_id.appspot.com/_ah/api');
+		}, ROOT_PATH);
 
 		var mapOptions = {
           center: new google.maps.LatLng(-34.397, 150.644),
