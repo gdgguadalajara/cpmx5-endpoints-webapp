d5f565d64c224b2b8e7e126210c4c1c24c7e8d47
Josue David Hernandez Gutierrez <megadavidtor@gmail.com>
2014-06-25 01:12:53 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+app = angular.module('app', []);
+
+
+
+app.controller('AppCtrl', ['$scope', '$window', function($scope, $window){
+	ctx = {
+		user_id: "",
+		google_locations: [
+
+		]
+	};
+
+	$window.init= function() {
+		$scope.$apply($scope.load_guestbook_lib);
+	};
+
+	ctx.get_data = function(){
+		gapi.client.coordinates.leer_coordenadas({id_usuario: ctx.user_id}).execute(function(resp){
+			console.log(resp);
+		}, '/_ah/spi');
+	}
+	
+	ctx.set_location = function(){
+		gapi.client.load('coordinates', 'v1', function() {
+			ctx.get_data();
+		});
+
+		var mapOptions = {
+          center: new google.maps.LatLng(-34.397, 150.644),
+          zoom: 8,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map_canvas"),
+            mapOptions);
+	}
+
+	$scope.Ctrl = ctx;
+}]);
